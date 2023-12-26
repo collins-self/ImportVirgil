020ff6f031ec753097ce982bb5c87cdbef2fe576
collins-self <collinsnater03@gmail.com>
2023-12-26 10:41:46 -0500

    Initial

---
+++ src/AllOfVirg.java
@@ -0,0 +1,9 @@
+/* Collins Self 12/26/23
+ * Grabs all the Virgil from this website: https://www.thelatinlibrary.com/verg.html
+ * And then converts it to a .txt file WITHOUT THE pesky line numbers
+ */
+public class AllOfVirg {
+    public static void main(String[] args) {
+
+    }
+}
