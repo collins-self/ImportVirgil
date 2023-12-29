4c37a1f3bc21de180c42fe263c3e71274a11f1e5
collins-self <collinsnater03@gmail.com>
2023-12-29 10:06:48 -0500

    updated ide files and GetAllPoems outline

--- src/GetAllPoems.java
+++ src/GetAllPoems.java
@@ -1,48 +1,46 @@
+//package com.collins-self.line-of-virgil.src.GetAllPoems;
+
 import javax.net.ssl.HttpsURLConnection;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 
-/* Collins Self 12/26/23
- * Grabs all the Virgil from this website: https://www.thelatinlibrary.com/verg.html
- * And then converts it to a .txt file WITHOUT THE pesky line numbers
- */
 public class GetAllPoems {
-    public static void main(String[] args) {
-        private static final String USER_AGENT = "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 " +
-                "(KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36";
-        private static final String URL = "https://" + "example.com";
+    private static final String USER_AGENT = "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 " +
+            "(KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36";
 
-        public static void SendGET() throws IOException {
-            java.net.URL url = new URL(URL);
-            // A URLConnection() is made, and cast as an HttpsURLConnection
-            HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
-            connection.setRequestMethod("GET"); // NOTE: GET is also the default method
-            connection.setRequestProperty("User-Agent", USER_AGENT);
-            int resCode = connection.getResponseCode();
-            System.out.println("GET Response code : " + resCode);
+    public static void SendGET(String ProvidedUrl) throws IOException {
+        // A URLConnection() is made, and cast as an HttpsURLConnection
+        URL url = new URL(ProvidedUrl);
+        HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
 
-            if (resCode == HttpsURLConnection.HTTP_OK) { // success
-                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-                String inputLine;
-                StringBuffer response = new StringBuffer();
+        connection.setRequestMethod("GET"); // NOTE: GET is also the default method
+        connection.setRequestProperty("User-Agent", USER_AGENT);
+        int resCode = connection.getResponseCode();
 
-                while ((inputLine = in.readLine()) != null) { // line read is not null
-                    response.append(inputLine);
-                }
-                in.close();
+        System.out.println("GET Response code : " + resCode);
 
-                // print result
-                System.out.println(response);
-            } else {
-                System.out.println("GET request did not work.");
+        if (resCode == HttpsURLConnection.HTTP_OK) { // success
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+
+            while ((inputLine = in.readLine()) != null) { // line read is not null
+                response.append(inputLine);
             }
-        }
+            in.close();
 
-        public static void main(String[] args) throws IOException {
-            SendGET();
+            // print result
+            System.out.println(response);
+        } else {
+            System.out.println("GET request did not work.");
         }
     }
+    public static void main(String[] args) throws IOException {
+       for (int i = 1; i < 13; ++i) {
+           String url = "https://https://www.thelatinlibrary.com/vergil/aen" + i + ".shtml";
+           SendGET(url);
+       }
     }
 }
